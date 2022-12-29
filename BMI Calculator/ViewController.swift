e44b48a758165a53c6b0feb2ea9eae01e4b05607
YojaLeonOficial <yojairoleonoficial@gmail.com>
2022-12-29 14:26:27 +0100

    Initial Commit

---
+++ BMI Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMI Calculator
+//
+//  Created by Yojairo Jose Leon Escobar on 29/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
